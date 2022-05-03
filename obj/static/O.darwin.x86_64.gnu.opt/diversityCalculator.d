6cfb0eca56bac5567798997058e7f432237d6f8e
Izuwa Ahanor <izuwaahanor@HughMBP.local>
2022-05-02 23:53:01 -0400

    made changes to code base. Using estimate in like Best first

--- obj/static/O.darwin.x86_64.gnu.opt/diversityCalculator.d
+++
@@ -1,3 +0,0 @@
-obj/static/O.darwin.x86_64.gnu.opt/diversityCalculator.o: \
-  src/diversityCalculator.cpp src/diversityCalculator.h \
-  src/solutionStoreVector.h src/utilities.h src/currentStateVariables.h
--- obj/static/O.darwin.x86_64.gnu.opt/solutionStoreVector.d
+++
@@ -1,2 +0,0 @@
-obj/static/O.darwin.x86_64.gnu.opt/solutionStoreVector.o: \
-  src/solutionStoreVector.cpp src/solutionStoreVector.h
