a8c0b2f52ad74ff4b9dc94169cc83a061c1e4445
Izuwa Ahanor <izuwaahanor@HughMBP.local>
2022-05-12 17:34:36 -0400

    Adding new train files

---
+++ obj/static/O.darwin.x86_64.gnu.opt/solutionsGenerator.d
@@ -0,0 +1,4 @@
+obj/static/O.darwin.x86_64.gnu.opt/solutionsGenerator.o: \
+  src/solutionsGenerator.cpp src/solutionsGenerator.h \
+  src/optionsReader.h src/oneTreeGams.h src/solutionStoreVector.h \
+  src/currentStateVariables.h
